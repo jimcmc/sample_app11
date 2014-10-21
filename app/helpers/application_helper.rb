e4ed36211ca76d399dc5e760a545292283d4f922
jimcmc <csos666@126.com>
2014-10-21 16:44:40 +0800

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def full_title(page_title)
+      basetitle = "Ruby on Rails Tutorial Sample App"
+      if page_title.empty?
+      	basetitle
+      else
+      	"#{basetitle} | #{page_title}"
+      end
+	end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
+
   describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
@@ -37,6 +40,14 @@ describe "Static pages" do
     	expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
     end
   end
+
+  describe "Contact page" do
+    it "should have the title 'Contact'" do
+    	visit '/static_pages/contact'
+    	expect(page).to have_title("#{base_title} | Contact")
+    end
+  end
+
 
 
 end
